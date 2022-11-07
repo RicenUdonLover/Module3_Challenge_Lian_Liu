0fa6346c066048a02bed5d36eae829b55015693e
Lian Liu <riceudon@gmail.com>
2022-11-06 22:32:51 -0800

    The perfect version

--- script.js
+++ script.js
@@ -10,21 +10,7 @@ const lowerCase = "abcdefghijklmnopqrstuvwxyz"
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numerals = "0123456789"
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-// var rdmLowercase = Math.floor(Math.random() * lowerCase.length);
-// var rdmUppercase = Math.floor(Math.random() * upperCase.length);
-// var rdmNumerals = Math.floor(Math.random() * numerals.length);
-// var rdmSpecialCharacters = Math.floor(Math.random() * specialCharacters.length);
 
-// var lowerCaseLetters = /[a-z]/g;
-// var upperCaseLetters = /[A-Z]/g;
-// var numbers = /[0-9]/g;
-// var symbols = /[!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
-
-
-// console.log(rdmGenerator)
-// console.log(lowerCase)
-// console.log(upperCase)
-// -----------------------------------------------------------------------------------------------------------------------------------
 // This is the life saver. This randomly selects an element in an array if it was selected by the user if the password length is less than desired length.
 var rdmCharacter = function (finalPassword, passwordLength, checking, array, range) {
   if (checking === true && finalPassword.length < passwordLength) {
@@ -37,14 +23,8 @@ var rdmCharacter = function (finalPassword, passwordLength, checking, array, ran
 
 // The function to generate password
 var generatePassword = function () {
+  var passwordPool = ""
   var passwordLength = window.prompt("How long is your password? \n<Minimum 8 charactors>\n<Maximun 128 charactors>");
-
-  // ---------------------Some random things, I used very bad syntax to check if the input if valid.----------------------
-  // var lengthNumber = Number.isNaN(passwordLength);
-  // console.log(lengthNumber)
-  // console.log(passwordLength)
-  // console.log(typeof passwordLength)    
-  // --------------------------------------------------------------------------------------------------------------
 
   // Check for invalid input 
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
@@ -54,64 +34,50 @@ var generatePassword = function () {
     // console.log(typeof passwordLength)    
   }
   // Collect user wanted criterias
+  var intitialCounter = 0
   var lcConfirm = window.confirm("Got it. Do you want <lowercase letters> in your password?");
   var ucConfirm = window.confirm("Not a problem. Do you want <UPPERCASE LETTERS> in your password?");
   var nbrConfirm = window.confirm("<Numbers> are always good in a password, do you want some?");
   var symbolConfirm = window.confirm("Sure. What about some <Spec!al ch@racters>?");
 
   // Check for invalid selection
-  if (
-    lcConfirm === false &&
-    ucConfirm === false &&
-    nbrConfirm === false &&
-    symbolConfirm === false
-  ) {
+  if (lcConfirm) {
+    passwordPool += lowerCase;
+    intitialCounter = intitialCounter + 1
+  }
+  if (ucConfirm) {
+    passwordPool += upperCase;
+    intitialCounter = intitialCounter + 1
+  }
+  if (nbrConfirm) {
+    passwordPool += numerals;
+    intitialCounter = intitialCounter + 1
+  }
+  if (symbolConfirm) {
+    passwordPool += specialCharacters;
+    intitialCounter = intitialCounter + 1
+  }
+  if (passwordPool.length == 0) {
     window.alert("Please choose at least one of the criterias.");
     return null;
   }
-
+  console.log(passwordPool)
+  console.log(intitialCounter)
   // Define and start to generate password
   var finalPassword = "";
   //  Another life saver. It will fill the password with user wanted criterias first so that at least 1 element of the selected criteria will be added to the password
-  for (let i = 0; i <= passwordLength; i++) {
-
-    finalPassword = rdmCharacter(finalPassword, passwordLength, lcConfirm, lowerCase, 26)
-    finalPassword = rdmCharacter(finalPassword, passwordLength, ucConfirm, upperCase, 26)
-    finalPassword = rdmCharacter(finalPassword, passwordLength, nbrConfirm, numerals, 10)
-    finalPassword = rdmCharacter(finalPassword, passwordLength, symbolConfirm, specialCharacters, specialCharacters.length)
-
-    console.log(finalPassword)
-    // shuffle the password to make it more random
-    var shuffled = finalPassword.split('').sort(function(){return 0.5-Math.random()}).join('');
-    // -------------------------------------------Random things. I tried to check for invalid passwords and use while loop to re-generate a password and it turns out to be very stupid.
-    // var numberValid = password.match(numbers)
-    // console.log(numberValid)
-    // if (numberValid == null) {
-    //   console.log("invalid")
-    // }
-    // if (lcConfirm) {
-    //   var lcValid = password.match(lowerCaseLetters)
-    //   console.log(lcValid)
-    //   if (lcValid == null) {
-    //     console.log("invalid")
-    //   }
-    // }
-    // if (ucConfirm) {
-    //   var ucValid = password.match(upperCaseLetters)
-    //   console.log(ucValid)
-    //   if (ucValid == null) {
-    //     console.log("invalid")
-    //   }
-    // }
-    // if (symbolConfirm) {
-    //   var symbolValid = password.match(symbols)
-    //   console.log(symbolValid)
-    //   if (symbolValid == null) {
-    //     console.log("invalid")
-    //   }
-    // }
-    // ----------------------------------------------------------------------------------------------------
+  //  First fill in 1 of each chosen charactor type
+  finalPassword = rdmCharacter(finalPassword, passwordLength, lcConfirm, lowerCase, 26)
+  finalPassword = rdmCharacter(finalPassword, passwordLength, ucConfirm, upperCase, 26)
+  finalPassword = rdmCharacter(finalPassword, passwordLength, nbrConfirm, numerals, 10)
+  finalPassword = rdmCharacter(finalPassword, passwordLength, symbolConfirm, specialCharacters, specialCharacters.length)
+  console.log(finalPassword)
+  // Then fill with random charaters from character pool
+  for (let i = intitialCounter; i < passwordLength; i++) {
+    finalPassword += passwordPool.charAt(Math.floor(Math.random() * passwordPool.length))
   }
+  // shuffle the password to make it more random
+  var shuffled = finalPassword.split('').sort(function () { return 0.5 - Math.random() }).join('');
   return shuffled
 }
 
@@ -123,8 +89,8 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  
-// A little add-on: copy the password to your clipboard.------------------------------------------------
+
+  // A little add-on: copy the password to your clipboard.------------------------------------------------
   var copyText = document.getElementById("password");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
